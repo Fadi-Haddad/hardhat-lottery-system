04791f3973951b35701d88acd01126fef4496f5a
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-01 15:42:43 +0300

    Create contract, add constructor and enterLottery function

---
+++ contracts/lottery.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract lettery {
+
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee){
+        entranceFee = i_entranceFee;
+    }
+
+    function enterLottery()  {
+        
+    }
+    // function pickRandomWinner(){}
+}
