64cc7d4fe4018c56acfe8b14c06b1428f5809014
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-01 15:58:53 +0300

    Add getPlayer function

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,20 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-Lottery__NotEnoughEthEntered()
+error Lottery__NotEnoughEthEntered()
 
 contract lettery {
-
-    uint256 private immutable i_entranceFee;
+    // State variables
+    uint256 private immutable i_entranceFee;    // i_ is used with immutable variables
+    address payable[] private s_players         // one player should win so addresses should be payable. s_ is used with storage variables
 
     constructor(uint256 entranceFee){
         entranceFee = i_entranceFee;
     }
 
-    function enterLottery()  {
+    function enterLottery() public payable {
         // require(msg.value > i_entranceFee, "Not enough ETH") this is not gas efficient, instead we can use if with a custom error
         if(msg.value< i_entranceFee) {revert Lottery__NotEnoughEthEntered()}
         
+    }
+    function getPlayer(uint256 index) public view returns(address) {
+        return s_players[index]
     }
     // function pickRandomWinner(){}
 }
