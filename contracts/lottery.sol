9a8097caf070f9aa101387b9d1f811fabc0435ff
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-01 15:49:10 +0300

    Replace require with if statement with a custom error to save gas

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+Lottery__NotEnoughEthEntered()
+
 contract lettery {
 
     uint256 private immutable i_entranceFee;
@@ -10,7 +12,8 @@ contract lettery {
     }
 
     function enterLottery()  {
-        require(msg.value > i_entranceFee, "Not enough ETH")
+        // require(msg.value > i_entranceFee, "Not enough ETH") this is not gas efficient, instead we can use if with a custom error
+        if(msg.value< i_entranceFee) {revert Lottery__NotEnoughEthEntered()}
         
     }
     // function pickRandomWinner(){}
