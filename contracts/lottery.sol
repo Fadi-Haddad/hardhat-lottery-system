3d361b4bf7046b785a4914cd75a068b8560facf1
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-02 21:24:22 +0300

    Import and inherit VRF ConsumeBaseV2 contract

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
 error Lottery__NotEnoughEthEntered()
 
-contract lettery {
+contract lettery is VRFConsumerBaseV2 {
     // State variables
     uint256 private immutable i_entranceFee;    // i_ is used with immutable variables
     address payable[] private s_players;         // one player should win so addresses should be payable. s_ is used with storage variables
 
-    constructor(uint256 entranceFee){
-        entranceFee = i_entranceFee;
-    }
-
-    function enterLottery() public payable {
+    constructor(address vrfCoordinatorv2, uint256 entranceFee) VRFConsumerBaseV2
         // require(msg.value > i_entranceFee, "Not enough ETH") this is not gas efficient, instead we can use if with a custom error
         if(msg.value< i_entranceFee) {revert Lottery__NotEnoughEthEntered()}
         s_players.push(payable(msg.sender));   // msg.sender is not payable so we need to typecast it
