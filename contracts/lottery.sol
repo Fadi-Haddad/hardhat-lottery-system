b75e357aab2cc8e0961e8c5760e60359bfce5066
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-02 22:21:43 +0300

    Import VRFCoordinatorV2Interface and prepare vrfCoordinator variable

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Lottery__NotEnoughEthEntered()
 
@@ -9,21 +10,23 @@ contract lettery is VRFConsumerBaseV2 {
     // State variables
     uint256 private immutable i_entranceFee;    // i_ is used with immutable variables
     address payable[] private s_players;         // one player should win so addresses should be payable. s_ is used with storage variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
 
 
     // events
     event LotteryEnter( address indexed player);
 
 
-    constructor(uint256 entranceFee){
+    constructor(uint256 entranceFee, address vrfCoordinatorV2){
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     }
 
     function enterLottery() public payable {
         // require(msg.value > i_entranceFee, "Not enough ETH") this is not gas efficient, instead we can use if with a custom error
         if(msg.value< i_entranceFee) {revert Lottery__NotEnoughEthEntered();}
         s_players.push(payable(msg.sender));   // msg.sender is not payable so we need to typecast it
-        
+
         emit LotteryEnter(msg.sender);
         
     }
