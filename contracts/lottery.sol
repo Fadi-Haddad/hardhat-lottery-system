3fcc0d1ef215af62ae2fb788e73187ba31bf0afd
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-02 21:40:43 +0300

    Fix constructor and enterLottery function

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -10,12 +10,17 @@ contract lettery is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;    // i_ is used with immutable variables
     address payable[] private s_players;         // one player should win so addresses should be payable. s_ is used with storage variables
 
-    constructor(address vrfCoordinatorv2, uint256 entranceFee) VRFConsumerBaseV2
+    constructor(uint256 entranceFee){
+        i_entranceFee = entranceFee;
+    }
+    
+    function enterLottery() public payable {
         // require(msg.value > i_entranceFee, "Not enough ETH") this is not gas efficient, instead we can use if with a custom error
-        if(msg.value< i_entranceFee) {revert Lottery__NotEnoughEthEntered()}
+        if(msg.value< i_entranceFee) {revert Lottery__NotEnoughEthEntered();}
         s_players.push(payable(msg.sender));   // msg.sender is not payable so we need to typecast it
         
     }
+
     function getPlayer(uint256 index) public view returns(address) {
         return s_players[index];
     }
