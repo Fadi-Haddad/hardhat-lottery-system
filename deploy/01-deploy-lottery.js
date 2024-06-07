13b51ca6042e125659e7df5111ddfcc03f934eae
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-07 22:22:41 +0300

    Import libraries and define deployLottery function in 01-deploy-lottery.js

---
+++ deploy/01-deploy-lottery.js
@@ -0,0 +1,17 @@
+const { network } = require("hardhat");
+const {verify} = require ("../utils/verify")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+require("dotenv").config()
+
+async function deployFundMe({ deployments, getNamedAccounts }) {
+    
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    const lottery = await deploy("Lottery", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.waitConfirmations || 1,
+      });
+}
